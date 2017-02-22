d2367152bc8c30e7ca4e193055dfcb57560e9ec6
Laura Cristina Martinez <lcmartinez15@gmail.com>
2017-02-22 01:41:17 -0500

    autosave

---
+++ primero23.bash
@@ -0,0 +1,12 @@
+@echo off
+echo Hola mundo
+echo %ruta%
+echo %CD%
+cd D:\Dropbox (Consultoría)\LaunchProgram\3.Week MVP\Repositorio
+git init
+git branch
+git status
+git add .
+git commit -m "prueba commit3" 
+git push
+pause
